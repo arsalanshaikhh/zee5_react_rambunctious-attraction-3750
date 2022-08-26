748e43ab89ab7f95f82460204cadff8e0a02ba65
arsalanshaikhh <arsalan4coder@gmail.com>
2022-08-26 23:31:55 +0530

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+import {
+  Image,
+  Text,
+  Center,
+  Input,
+  Button,
+  Flex,
+  Square,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+import { HamburgerIcon, QuestionIcon, DragHandleIcon } from "@chakra-ui/icons";
+
+function Navbar() {
+  return (
+    // <div className={styles.navcon}>
+    //   <Box boxSize="sm">
+    //     <Image h='60px'
+    //       src="https://www.zee5.com/images/ZEE5_logo.svg?ver=2.51.88"
+    //       alt="Zee5"
+    //     />
+    //   </Box>
+    //   <Text fontSize="2*l">Home</Text>
+    //   <Text fontSize="2*l">Tv Shows</Text>
+    //   <Text fontSize="2*l">Movies</Text>
+
+    // </div>
+    <Flex
+      h="80px"
+      bg="rgb(15,6,23)"
+      p="2%"
+      w="100%"
+      justifyContent="space-between"
+    >
+      <Flex w="50%" justifyContent="space-evenly" alignItems="align">
+        <Center>
+          <Image
+            h="50px"
+            src="https://www.zee5.com/images/ZEE5_logo.svg?ver=2.51.88"
+            alt="Zee5"
+          />
+        </Center>
+
+        <Square color="white">
+          <Link to="/">Home</Link>
+        </Square>
+
+        <Square color="white">
+          <Link to="/tvshows">TV Shows</Link>
+        </Square>
+
+        <Square color="white">
+          <Text>Movies</Text>
+        </Square>
+
+        <Square color="white">
+          <Text>Web Series</Text>
+        </Square>
+
+        <Square color="white">
+          <Text>News</Text>
+        </Square>
+
+        <Square color="white">
+          <Text>Eduauraa</Text>
+        </Square>
+
+        <Square color="white">
+          <Text>Live TV</Text>
+        </Square>
+
+        <Square color="white">
+          <Text>Music</Text>
+        </Square>
+
+        <Square color="white">
+          <DragHandleIcon boxSize={5} />
+        </Square>
+      </Flex>
+
+      <Flex w="45%" justifyContent="space-evenly" alignItems="align">
+        <Square color="white" flexGrow="7">
+          <Input
+            isInvalid
+            errorBorderColor="white"
+            placeholder="Search For Movies, shows etc."
+            size="sm"
+          />
+        </Square>
+
+        <Square color="white" flexGrow="1">
+          <QuestionIcon boxSize={4} />
+        </Square>
+
+        <Square color="white" flexGrow="1">
+          <Button colorScheme="white" variant="outline" size="sm">
+            LOGIN
+          </Button>
+        </Square>
+
+        <Square color="white" flexGrow="1">
+          <Button colorScheme="purple" size="sm">
+            BUY PLAN
+          </Button>
+        </Square>
+
+        {/* <Square color="white" flexGrow="1">
+          <HamburgerIcon boxSize={6} />
+        </Square> */}
+
+        <Square color="white" flexGrow="1">
+          <HamburgerIcon boxSize={6} />
+        </Square>
+      </Flex>
+    </Flex>
+
+    // <Wrap w="100%" bg="red.200">
+    //   <WrapItem>
+    //     <Center w="100%" h="80px" bg="red.200">
+    //       <Image
+    //         h="60px"
+    //         src="https://www.zee5.com/images/ZEE5_logo.svg?ver=2.51.88"
+    //         alt="Zee5"
+    //       />
+    //     </Center>
+    //   </WrapItem>
+    //   <WrapItem>
+    //     <Center w="180px" h="80px" bg="green.200">
+    //       Home
+    //     </Center>
+    //   </WrapItem>
+    //   <WrapItem>
+    //     <Center w="180px" h="80px" bg="tomato">
+    //       Tv Shows
+    //     </Center>
+    //   </WrapItem>
+    //   <WrapItem>
+    //     <Center w="180px" h="80px" bg="blue.200">
+    //       Movies
+    //     </Center>
+    //   </WrapItem>
+    //   <WrapItem>
+    //     <Center w="180px" h="80px" bg="blue.200">
+    //       Box 4
+    //     </Center>
+    //   </WrapItem>
+    //   <WrapItem>
+    //     <Center w="180px" h="80px" bg="blue.200">
+    //       Box 4
+    //     </Center>
+    //   </WrapItem>
+    //   <WrapItem>
+    //     <Center w="180px" h="80px" bg="blue.200">
+    //       Box 4
+    //     </Center>
+    //   </WrapItem>
+    // </Wrap>
+  );
+}
+
+export default Navbar;
+
+//
---
+++ src/components/routes/Home.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import axios from "axios";
+import ProductAddToCart from "../Moviecard";
+import { CCarousel, CCarouselItem, CImage } from "@coreui/react";
+import { Alert } from "@coreui/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@coreui/coreui/dist/css/coreui.min.css";
+
+function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+  // var key = "AIzaSyAKo2_8Ji4nOCiN1uTPBhgenXAbYm9XBQs";
+  const getData = () => {
+    axios({
+      url: `https://pure-fortress-87288.herokuapp.com/bolly`,
+      method: "GET",
+    })
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  };
+  // console.log(data);
+  return (
+    <>
+      <div>
+        <CCarousel controls>
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src="https://wallpaperaccess.com/full/895201.jpg"
+              alt="Game of Thrones"
+            />
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100 h-80"
+              src="https://wallpaperaccess.com/full/637343.jpg"
+              alt="slide 1"
+            />
+          </CCarouselItem>
+          {/* <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src="https://wallpaperaccess.com/full/758165.jpg"
+              alt="slide 2"
+            />
+          </CCarouselItem> */}
+
+          <CCarouselItem>
+            <CImage
+              className="d-block w-100"
+              src="https://wallpaperaccess.com/full/895179.jpg"
+              alt="slide 3"
+            />
+          </CCarouselItem>
+        </CCarousel>
+      </div>
+      <div>
+        <Text pt="30px" fontSize="2xl" bg="rgb(15,6,23)" color="white">
+          Bollywood Movies
+        </Text>
+        <SimpleGrid
+          columns={[1, 3, 3, 6]}
+          spacing="2%"
+          p="3% 5%"
+          color="white"
+          bg="rgb(15,6,23)"
+          w="100%"
+        >
+          <>
+            {data.map((e) => {
+              return <ProductAddToCart key={e.id} props={e}></ProductAddToCart>;
+            })}
+          </>
+        </SimpleGrid>
+      </div>
+      <div>
+        <Text pt="30px" fontSize="2xl" bg="rgb(15,6,23)" color="white">
+          Bollywood Movies
+        </Text>
+        <SimpleGrid
+          columns={[1, 3, 3, 6]}
+          spacing="2%"
+          p="3% 5%"
+          color="white"
+          bg="rgb(15,6,23)"
+          w="100%"
+        >
+          <>
+            {data.map((e) => {
+              return <ProductAddToCart key={e.id} props={e}></ProductAddToCart>;
+            })}
+          </>
+        </SimpleGrid>
+      </div>
+      <div>
+        <Text pt="30px" fontSize="2xl" bg="rgb(15,6,23)" color="white">
+          Bollywood Movies
+        </Text>
+        <SimpleGrid
+          columns={[1, 3, 3, 6]}
+          spacing="2%"
+          p="3% 5%"
+          color="white"
+          bg="rgb(15,6,23)"
+          w="100%"
+        >
+          <>
+            {data.map((e) => {
+              return <ProductAddToCart key={e.id} props={e}></ProductAddToCart>;
+            })}
+          </>
+        </SimpleGrid>
+      </div>
+    </>
+  );
+}
+
+export default Home;
